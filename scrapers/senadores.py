f15147538a9e43a144bc3563c3e13f436dce561a
Antonio Ossa <aaossa@uc.cl>
2019-11-03 03:37:42 -0300

    :hammer: Senators scraper refactor
    Fix dependencies, add relevant comments and also generate a json file
    with the extracted data
    
    Signed-off-by: Antonio Ossa <aaossa@uc.cl>

--- scrapers/senadores.py
+++ scrapers/senadores.py
@@ -1,30 +1,59 @@
+import json
+import os
+
 import requests
 from bs4 import BeautifulSoup as bs
 
 
-req = requests.post(f"https://www.senado.cl/appsenado/templates/senadores/regiones_2018.html")
-soup = bs(req.text, "html.parser")
-detalle = soup.find("div", {"class": "mtabs"})
+BASE_URL = "https://www.senado.cl"
+DATA = {}
+OUTPUT_PATH = os.path.join("..", "data", "senadores.json")
 
-RIDS = [
-	"mtabrm" if i == 13
-	else f"mtab{i}"
-	for i in range(1, 16)
-]
 
-s = 0
-for rid in RIDS:
-	tab = detalle.find("div", {"id": rid})
-	hgroup = tab.hgroup
-	print(hgroup.h1.get_text())
-	print(hgroup.h2.get_text())
-	print(hgroup.h3.get_text())
-	for td in tab.find_all("td"):
-		if not td.find("h1"):
-			continue
-		s += 1
-		print(">", td.h1.get_text().split("\n")[0])
-		print(">>", td.img["src"])
-		print(">>", td.a["href"])
-	print()
-print(f"Información de {s} senadores")
+# Send request and extract elements
+req = requests.post(f"{BASE_URL}/appsenado/templates/senadores/regiones_2018.html")
+soup = bs(req.text, "html5lib")
+container = soup.find("div", {"class": "mtabs"})
+
+# Iterate elements inside container and extract structure and content
+elements = container.find_all("div")
+for element in elements:
+    if element.get("id") == "mtabinfo":
+        continue
+    # First group with region information
+    circumscription = element.hgroup.h1.get_text()
+    region = element.hgroup.h2.get_text()
+    region_capital = element.hgroup.h3.get_text()
+    DATA[region] = {
+        "circunscripcion": circumscription,
+        "capital": region_capital,
+    }
+    print(f"Circunscripción: {circumscription}")
+    print(f"Región: {region}")
+    print(f"Capital regional: {region_capital}")
+    # Second group with senators information
+    senators = []
+    print("Senadores:")
+    for senator in element.find_all("td"):
+        # Skip element if does not contain a senator information
+        if not senator.find("h1"):
+            continue
+        name = senator.h1.get_text()
+        senator_href = senator.a["href"]
+        senator_data = {
+            "nombre": name,
+            "url_img": BASE_URL + senator.img["src"],
+            "url_senador": BASE_URL + senator_href,
+        }
+        senators.append(senator_data)
+        print(f"* {name}")
+    DATA[region]["senadores"] = senators
+
+    print()
+
+# Store data in its own JSON file
+with open(OUTPUT_PATH, "w", encoding="utf-8") as senatores_file:
+    json.dump(DATA, senatores_file,
+              sort_keys=True, indent=4,
+              separators=(',', ': '), ensure_ascii=False,
+              )
